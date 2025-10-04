f500ece0993ca39a79853f2008fd7be6a9437a05
phegondev <phegondev@gmail.com>
2025-10-04 23:10:09 +0100

    project completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Profile from "./pages/Profile";
+import UpdateProfile from "./pages/UpdateProfile";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import Transactions from "./pages/Transactions";
+import Transfer from "./pages/Transfer";
+import AuditorDashboard from "./pages/AuditorDashboard";
+import { AuditorRoute, CustomerRoute } from "./services/Guard";
+import Deposit from "./pages/Deposit";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
+
+        <Route path="/profile" element={<CustomerRoute element={<Profile />} />} />
+        <Route path="/update-profile" element={<CustomerRoute element={<UpdateProfile />} />} />
+
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+
+        <Route path="/transactions" element={<CustomerRoute element={<Transactions />} />} />
+        <Route path="/transfer" element={<CustomerRoute element={<Transfer />} />} />
+
+
+        <Route path="/auditor-dashboard" element={<AuditorRoute element={<AuditorDashboard />} />} />
+        <Route path="/deposit" element={<AuditorRoute element={<Deposit />} />} />
+
+
+
+
+
+
+        {/* WILDCARD ROUTE */}
+        <Route path="*" element={<NotFound />} />
+
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
   );
 }
 
